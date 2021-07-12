e9186aa5b163143586281f74ab48cb9317e2b2b1
nomulavenkatramana <87078767+nomulavenkatramana@users.noreply.github.com>
2021-07-12 15:06:18 +0530

    assignment 4 submitted

---
+++ index.js
@@ -0,0 +1,57 @@
+// var ele=document.getElementsByTagName('p');
+// console.log(ele[0]);
+
+// var ele=document.getElementsByClassName('para');
+// console.log(ele);
+
+// var ele=document.getElementById('p1');
+// ele.innerText="Letsupgrade";
+
+// function doSomething(){
+
+//     var d=document.getElementById('one');
+//     d.innerHTML="<h1>Hello</h1>";
+
+// }
+
+
+function takeValue(){
+
+    var inp=document.getElementById('inp');
+    var data=inp.value;
+
+    document.getElementById('name').innerText=data;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
